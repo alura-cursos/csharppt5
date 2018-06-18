13867085aaed447d3fa591d84670b22031f61b9a
Guilherme Matheus Costa <costa.guimatheus@gmail.com>
2018-06-18 22:42:58 +0000

    Projeto da aula 5 - NuGet

---
+++ ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank.Modelos;
+using ByteBank.Modelos.Funcionarios;
+using Humanizer;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dataFimPagamento = new DateTime(2018, 6, 20);
+            DateTime dataCorrente = DateTime.Now;
+
+            TimeSpan diferenca = TimeSpan.FromMinutes(40); //dataFimPagamento - dataCorrente;
+
+            string mensagem = "Vencimento em " + TimeSpanHumanizeExtensions.Humanize(diferenca);
+
+            Console.WriteLine(mensagem);
+
+            Console.ReadLine();
+        }
+    }
+}
