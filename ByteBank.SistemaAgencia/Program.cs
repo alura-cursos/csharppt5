c30c8c4723be95e9b742e26ba80c1a8fdc06cc4b
Guilherme Matheus Costa <costa.guimatheus@gmail.com>
2018-06-18 22:42:38 +0000

    Projeto da aula 3 - Referencia de DLL

---
+++ ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank.Modelos;
+using ByteBank.Modelos.Funcionarios;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(847, 489754);
+
+
+            FuncionarioAutenticavel carlos = null;
+            carlos.Autenticar("kjhasfkjas");
+
+            Console.WriteLine(conta.Numero);
+
+            Console.ReadLine();
+        }
+    }
+}
