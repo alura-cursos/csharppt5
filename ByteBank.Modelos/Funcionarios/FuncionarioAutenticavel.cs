ca77771c690614e6ea320e463beec8e0ba873d3f
Guilherme Matheus Costa <costa.guimatheus@gmail.com>
2018-06-18 22:42:17 +0000

    Projeto da aula 1 - Projeto de biblioteca

---
+++ ByteBank.Modelos/Funcionarios/FuncionarioAutenticavel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank.Modelos;
+
+namespace ByteBank.Modelos.Funcionarios
+{
+    public abstract class FuncionarioAutenticavel : Funcionario, IAutenticavel
+    {
+        public string Senha { get; set; }
+
+        public FuncionarioAutenticavel(double salario, string cpf)
+            : base(salario, cpf)
+        {
+        }
+
+        public bool Autenticar(string senha)
+        {
+            return Senha == senha;
+        }
+    }
+}
---
+++ ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank.Modelos;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(847, 489754);
+
+            Console.WriteLine(conta.Numero);
+
+            Console.ReadLine();
+        }
+    }
+}
